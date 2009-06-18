88320a2ef254aeeee98f7a0e140674ced6600c36
madmanwoo <madmanwoo@8fa32c94-596e-11de-a527-d564076fe1eb>
2009-06-18 07:10:27 +0000

    Added internal TableCommandRunner, used to run table based commands against Mapinfo. Top level API will talk to CommandRunners rather then IMapinfoWrapper directly. Added TableFactory, used to create new tables or open tables from path.

---
+++ trunk/MapinfoWrapper.Core/trunk/src/Core/Internals/ITableCommandRunner.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MapinfoWrapper.Core.Internals
+{
+    internal interface ITableCommandRunner
+    {
+        string GetName(int tableNumber);
+
+        void OpenTable(string p);
+    }
+}
---
+++ trunk/MapinfoWrapper.Core/trunk/src/Core/Internals/TableCommandRunner.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MapinfoWrapper.Mapinfo;
+using MapinfoWrapper.Core.IoC;
+using MapinfoWrapper.Core.Extensions;
+using MapinfoWrapper.TableOperations;
+
+namespace MapinfoWrapper.Core.Internals
+{
+    internal class TableCommandRunner : ITableCommandRunner
+    {
+        private IMapinfoWrapper wrapper;
+
+        public TableCommandRunner() 
+            : this(IoC.IoC.Resolve<IMapinfoWrapper>())
+        { }
+
+        public TableCommandRunner(IMapinfoWrapper mapinfo)
+        {
+            this.wrapper = mapinfo;
+        }
+
+        public string GetName(int tableNumber)
+        {
+            int enumValue = (int)TableInfo.Name;
+            string command = "TableInfo(0,{0})".FormatWith(enumValue);
+            return this.wrapper.Evaluate(command);
+        }
+
+        public void OpenTable(string tablePath)
+        {
+            this.wrapper.RunCommand("Open Table {0}".FormatWith(tablePath.InQuotes()));
+        }
+    }
+}
