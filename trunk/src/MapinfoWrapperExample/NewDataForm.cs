7a9bea9f18845a3f9d75bb6f0ff057d79a706687
madmanwoo <madmanwoo@8fa32c94-596e-11de-a527-d564076fe1eb>
2009-06-28 10:01:26 +0000

    Updated example to work with version 1.0.0.0 of MapinfoWrapper.

---
+++ trunk/src/MapinfoWrapperExample/NewDataForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Forms;
+using Wrapper.Example.Tables;
+
+namespace Wrapper.Example
+{
+    public partial class NewDataForm : Form
+    {
+        public NewDataForm()
+        {
+            InitializeComponent();
+        }
+
+        public AddResult Show(World worldData)
+        {
+            this.worldBindingSource.DataSource = worldData;
+            if (this.ShowDialog() == DialogResult.OK)
+            {                
+                AddResult returnresult = new AddResult();
+                returnresult.Result = DialogResult.OK;
+                returnresult.EditedData = worldData;
+                return returnresult;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public class AddResult
+        {
+            public DialogResult Result { get; set; }
+            public World EditedData { get; set; }
+        }
+
+        private void OkButton_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+        }
+    }
+}
--- trunk/src/MapinfoWrapperExample/UsesWrapperAttribute.cs
+++ trunk/src/MapinfoWrapperExample/UsesWrapperAttribute.cs
@@ -6,7 +6,7 @@ namespace Wrapper.Example
     /// A attribute to mark that the method uses the Mapinfo OLE Wrapper.
     /// 
     /// This attribute is just for information purposes and doesn't do anything
-    /// apart from to show which method use the wrapper and which ones don't.
+    /// apart from to show which methods use the wrapper and which ones don't.
     /// </summary>
     public class UsesWrapperAttribute : Attribute
     {
