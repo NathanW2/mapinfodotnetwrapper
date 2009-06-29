795a4a77691301a5ad77aa40993a765421bb506d
Nathan Woodrow <NathanW2@users.noreply.github.com>
2009-06-29 05:53:46 +0000

    Fixed bug where user couldn't create GeometryFactory, should have been internal. Exposed factory methods on Line,Point. Updated version to 1.0.2.0

--- MapinfoWrapper.Core/trunk/src/Wrapper/Geometries/Points/Point.cs
+++ MapinfoWrapper.Core/trunk/src/Wrapper/Geometries/Points/Point.cs
@@ -6,6 +6,7 @@ using MapinfoWrapper.MapbasicOperations;
 using MapinfoWrapper.Core.Extensions;
 using MapinfoWrapper.Core.IoC;
 using MapinfoWrapper.Mapinfo;
+using MapinfoWrapper.Wrapper.Geometries;
 
 namespace MapinfoWrapper.Geometries.Points
 {
@@ -13,5 +14,20 @@ namespace MapinfoWrapper.Geometries.Points
     {
     	public Point(IVariable variable)
     		: base(variable) { }
+
+        /// <summary>
+        /// Creates a new point object in Mapinfo. 
+        /// Returns a <see cref="T:MapinfoWrapper.Geometries.Points.Point"/> which can be inserted into a
+        /// <see cref="T:MapinfoWrapper.DataAccess.Table"/>
+        /// <para>This function will create a new object variable in Mapinfo with a modified GUID as its name.</para>
+        /// </summary>
+        /// <param name="location">A <see cref="T:MapinfoWrapper.Geometries.Coordinate"/> that contains coordinates at
+        /// which to create the point.</param>
+        /// <returns>A new point object.</returns>
+        public static Point CreatePoint(Coordinate location)
+        {
+            IGeometryFactory geofactory = ServiceLocator.GetInstance<IFactoryBuilder>().BuildGeomtryFactory();
+            return (Point)geofactory.CreatePoint(location);
+        }
     }
 }
